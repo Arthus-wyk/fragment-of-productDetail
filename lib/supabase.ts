91da5884238073d3baf4739508ec072c7f697aed
zhangyirong111 <2963342564@qq.com>
2024-12-13 16:30:04 +0800

    修改环境变量

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = process.env.NEXT_PUBLIC_ENABLE_SUPABASE
   ? createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      'https://vkkmuixykvprdmemevnn.supabase.co',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZra211aXh5a3ZwcmRtZW1ldm5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgzNjYxNzMsImV4cCI6MjAzMzk0MjE3M30.W_obDMONlvR7ZXD9IvZZiDrEBCagZne0PVq0tJFER8k',
     )
   : undefined
