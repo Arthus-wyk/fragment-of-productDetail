4ff3472de740281efe1c4ebc8d23232ffd8da21c
DEVELOPER\29633 <wangyikai@wellekq.com>
2025-02-27 10:19:03 +0800

    安装vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Suspense } from 'react'
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
