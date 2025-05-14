113386fa10fa6e572475351108eefb1c279f2a95
wyk <2963342564@qq.com>
2025-05-14 21:10:05 +0800

    登录失败

--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -59,7 +59,6 @@ function AuthForm({
           theme="default"
           showLinks={true}
           providers={['github', 'google']}
-          redirectTo="https://fragment-of-product-detail-dveq.vercel.app/web-generator/"
           providerScopes={{
             github: 'email',
           }}
